ddfd5d439186bfe1319702932ec70b1e023fc12a
Tristan SWART <tswart@c4r23s3.wethinkcode.co.za>
2019-11-04 13:39:44 +0200

    First Upload

---
+++ setup.php
@@ -0,0 +1,28 @@
+<?php
+    $DB_HOST = 'localhost';
+    $DB_PASSWORD = 'tr15tan';
+    $DB_ROOT = 'root';
+    $DB_NAME = 'test';
+    
+    try{
+        $conn = new PDO("mysql:host=$DB_HOST", $DB_ROOT, $DB_PASSWORD);
+        $conn->query("USE $DB_NAME");
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+        $sql = $conn->prepare("CREATE DATABASE IF NOT EXISTS test");
+        $sql->execute();
+        
+        $sql = $conn->prepare('CREATE TABLE IF NOT EXISTS `users` (
+                 `id` INT  NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                 `name` VARCHAR(255) NOT NULL,
+                 `email` VARCHAR(255) NOT NULL,
+                 `password` VARCHAR(255) NOT NULL);');
+        $sql->execute();
+        
+    } 
+    catch(PDOException $fish)
+    {
+        echo "Error: " . $fish->getMessage();
+    }
+
+?>
---
+++ signin.php
@@ -0,0 +1,30 @@
+<?php
+
+  include 'data.php';
+
+  session_start();
+  if (isset($_POST)) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = hash('md5', $_POST['password']);
+    print_r($_POST);
+    printf("name: %s\n email: %s \npassword: %s ", $name, $email, $password);
+    
+    $sql = $conn->prepare('SELECT COUNT(*) FROM users WHERE email=?');
+    $sql->execute([$email]);
+    $res = $sql->fetchColumn();
+    printf($res);
+    if ($res > 0) {
+      echo ("TAKEN B*TCH");
+    }
+
+    else {
+        $sql = $conn->prepare('INSERT INTO users (`name`, `email`, `password`) VALUES (:name, :email, :password);');
+        $sql->bindParam(':name', $name);
+        $sql->bindParam(':email', $email);
+        $sql->bindParam(':password', $password);
+        $sql->execute();
+      }    
+    //   add to DB
+  }
+?>
